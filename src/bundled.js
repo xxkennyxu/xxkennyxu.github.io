fdb6eea7ba9c05ff61b5cdca63abc669a9b4d0fb
Kenny Xu <xxkennyxu@gmail.com>
2022-09-17 19:32:48 -0700

    push

--- src/bundled.js
+++ src/bundled.js
@@ -616,8 +616,8 @@ var SoloLocation = /** @class */ (function (_super) {
             }
         }
         // if no boss is spawning soon and we considered the data from AlData, switch server back if applicable
-        if (!bossSpawningSoon && ("PVP" != server.id || "US" != server.region)) {
-            changeServer("US", "PVP");
+        if (!bossSpawningSoon && ("I" != server.id || "US" != server.region)) {
+            changeServer("US", "I");
         }
         if (!nextLocation) {
             if (typeof this.mobDestination === "string") {
